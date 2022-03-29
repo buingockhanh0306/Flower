728619e604a8355267caa0d4132a5ba765154fcc
khanh <buikhanhi2k@gmail.com>
2022-03-29 15:16:03 +0700

    fix search

--- src/Component/Molecules/GroupIcons/index.js
+++ src/Component/Molecules/GroupIcons/index.js
@@ -1,115 +1,121 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import './style.css'
-import { signOutUser } from '../../../firebase-config'
-import { auth } from '../../../firebase-config';
-import { onAuthStateChanged } from 'firebase/auth'
-import { useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./style.css";
+import { signOutUser } from "../../../firebase-config";
+import { auth } from "../../../firebase-config";
+import { onAuthStateChanged } from "firebase/auth";
+import { useSelector } from "react-redux";
 
 function GroupIcons(props) {
-    const navigate = useNavigate();
-    const cartSelector = useSelector((state) => state.cart.cart)
-    const [user, setUser] = useState({})
+  const navigate = useNavigate();
+  const cartSelector = useSelector((state) => state.cart.cart);
+  const [active, setActive] = useState(false);
+  const [user, setUser] = useState({});
 
-    onAuthStateChanged(auth, (curentUser) => {
-        setUser(curentUser)
-    })
+  onAuthStateChanged(auth, (curentUser) => {
+    setUser(curentUser);
+  });
 
-    const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
-    const changeURL = () => {
-        navigate(`/checkout`)
+  const changeURL = () => {
+    navigate(`/checkout`);
+  };
+
+  const handleSearch = () => {
+    navigate(`/search/${value}`);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setValue(event.target.value.trim());
+      handleSearch();
     }
+  };
+  return (
+    <>
+      <div className="group-icon-header">
+        <div className={`input-search-group ${active ? "open" : "close"}`}>
+          <input
+            onKeyPress={(e) => handleKeyPress(e)}
+            onChange={(e) => setValue(e.target.value.trim())}
+            className="input-search"
+            placeholder="Search..."
+          />
+          <button onClick={() => handleSearch()} className="search-btn">
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
 
-    const handleSearch = () => {
-        navigate(`/search/${value}`)
+        <button className="search-icon">
+          <i
+            onClick={() => setActive(true)}
+            className={`fas fa-search ${active ? "close" : "open"}`}
+          ></i>
+          <i
+            onClick={() => setActive(false)}
+            className={`diss fa-solid fa-xmark ${active ? "open" : "close"}`}
+          ></i>
+        </button>
 
-    }
+        <button onClick={() => changeURL()} className="cart-icon">
+          <div className="cart">
+            <i className="fas fa-shopping-cart"></i>
+            {cartSelector.length === 0 || (
+              <div className="circle-red">{cartSelector.length}</div>
+            )}
+          </div>
+        </button>
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            setValue(event.target.value.trim())
-            handleSearch()
-        }
-    }
-    const handleHiddenSearch = () => {
-        const input_search_group = document.querySelector('.input-search-group')
-        const input_search = document.querySelector('.input-search')
-        const open = document.querySelector('.open')
-        const close = document.querySelector('.close')
-        const active = document.querySelector(".search-active")
-        console.log(active);
-        if (active === null) {
-            console.log('vao day ');
-            open.style.display = "none";
-            close.style.display = "block";
-            input_search_group.classList.add("search-active")
-            input_search.focus()
-        }
-        else {
-            open.style.display = "block";
-            close.style.display = "none";
-            input_search_group.classList.remove("search-active")
-        }
-    }
-    return (
-        <>
-            <div className='group-icon-header'>
-                <div className='input-search-group'>
-                    <input onKeyPress={(e) => handleKeyPress(e)} onChange={e => setValue(e.target.value.trim())} className='input-search' placeholder='Search...' />
-                    <button onClick={() => handleSearch()} className='search-btn'><i className="fas fa-search"></i></button>
-                </div>
+        <div className="avatar-user">
+          {user?.photoURL ? (
+            <img className="avatar-user-img" src={user?.photoURL} />
+          ) : (
+            <img className="avatar-user-img" src="assets/images/avatar.png" />
+          )}
+          <div className="login-logout">
+            {user?.displayName || user?.email ? (
+              <>
+                <span className="login-logout-item">
+                  <img src={user?.photoURL || "assets/images/avatar.png"} />
+                  <span className="name-user">
+                    {user?.displayName || user.email}
+                  </span>
+                </span>
+                <hr />
+                <span
+                  onClick={() => navigate("/login")}
+                  className="login-logout-item"
+                >
+                  <i className="fa-solid fa-arrow-right-arrow-left"></i>
+                  Change Account
+                </span>
 
-
-                <button onClick={() => handleHiddenSearch()} className='search-icon'>
-                    <i className="open fas fa-search"></i>
-                    <i className="close fa-solid fa-xmark"></i>
-                </button>
-
-                <button onClick={() => changeURL()} className='cart-icon'>
-                    <div className='cart'>
-                        <i className="fas fa-shopping-cart"></i>
-                        {cartSelector.length === 0 || <div className='circle-red'>{cartSelector.length}</div>}
-                    </div>
-                </button>
-
-                <div className='avatar-user'>
-                    {user?.photoURL ? <img className='avatar-user-img' src={user?.photoURL} /> : <img className='avatar-user-img' src='assets/images/avatar.png' />}
-                    <div className='login-logout'>
-                        {user?.displayName || user?.email ?
-                            <>
-                                <span className='login-logout-item'>
-                                    <img src={user?.photoURL || 'assets/images/avatar.png'} />
-                                    <span className='name-user'>{user?.displayName || user.email}</span>
-                                </span>
-                                <hr />
-                                <span onClick={() => navigate('/login')} className='login-logout-item'>
-                                    <i className="fa-solid fa-arrow-right-arrow-left"></i>
-                                    Change Account
-                                </span>
-
-                                <span onClick={signOutUser} className='login-logout-item logout'>
-                                    <i className="fa-solid fa-arrow-right-from-bracket"></i>
-                                    Log out
-                                </span>
-                            </> :
-                            <>
-                                <span onClick={() => navigate('/login')} className='login-logout-item'>
-                                    <i className="fa-solid fa-right-to-bracket"></i>
-                                    Log in
-                                </span>
-                            </>
-                        }
-                    </div>
-
-
-
-                </div>
-
-            </div>
-        </>
-    );
+                <span
+                  onClick={signOutUser}
+                  className="login-logout-item logout"
+                >
+                  <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                  Log out
+                </span>
+              </>
+            ) : (
+              <>
+                <span
+                  onClick={() => navigate("/login")}
+                  className="login-logout-item"
+                >
+                  <i className="fa-solid fa-right-to-bracket"></i>
+                  Log in
+                </span>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default GroupIcons;
--- src/Component/Molecules/ProductInfo/index.js
+++ src/Component/Molecules/ProductInfo/index.js
@@ -1,83 +1,81 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, useNavigate, useLocation, useParams } from "react-router-dom";
-import { useState } from 'react/cjs/react.development';
-import PlusItem from '../../Atoms/PlusItem';
-import Price from '../../Atoms/Price';
-import './style.css'
-import Heading from '../../Atoms/Heading';
-import ButtonColor from '../ButtonColor';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../../../redux/cart/cartAction';
-import { db } from "../../../firebase-config.js"
-import { collection, getDocs } from "firebase/firestore"
+import React, { useEffect } from "react";
+import { BrowserRouter, useNavigate, useParams } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
+import PlusItem from "../../Atoms/PlusItem";
+import Price from "../../Atoms/Price";
+import "./style.css";
+import Heading from "../../Atoms/Heading";
+import ButtonColor from "../ButtonColor";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../redux/cart/cartAction";
+import { db } from "../../../firebase-config.js";
+import { collection, getDocs } from "firebase/firestore";
 
 function ProductInfo(props) {
-    const [flowers, setFlower] = useState([])
-    const productColectionRef = collection(db, "products")
+  const [flowers, setFlower] = useState([]);
+  const productColectionRef = collection(db, "products");
 
-    const dispatch = useDispatch()
-    const notify = () => toast("Đã thêm vào giỏ hàng");
+  const dispatch = useDispatch();
+  const notify = () => toast("Đã thêm vào giỏ hàng");
 
-    const {id} = useParams();
+  const { id } = useParams();
 
-    useEffect(() => {
-        const getFlowers = async () => {
-            const data = await getDocs(productColectionRef);
-            setFlower(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })).filter(doc=>doc.id===id));
-        };
-        getFlowers();
-    }, []);
+  useEffect(() => {
+    const getFlowers = async () => {
+      const data = await getDocs(productColectionRef);
+      setFlower(
+        data.docs
+          .map((doc) => ({ ...doc.data(), id: doc.id }))
+          .filter((doc) => doc.id === id)
+      );
+    };
+    getFlowers();
+  }, []);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    // REDUX
-    const handleAddProduct = (flower) => {
-        dispatch(addToCart(flower))
-        notify();
-    }
+  // REDUX
+  const handleAddProduct = (flower) => {
+    dispatch(addToCart(flower));
+    notify();
+  };
 
-    const changeURL = (id) => {
-        navigate(`/checkout`)
-    }
-    const renderFlower = () => {
-        return (
-            flowers.map(flower =>
-            (
-                <div key={flower.id}>
-                    <Heading categoryName={flower.name} text={flower.name} />
-                    <Price priceNew={flower.price} />
-                    <div className='line'></div>
-                    {/* <CountGroup /> */}
-                    <div>Color: </div>
-                    <ButtonColor />
-                    <div className='order'>
-                        <button onClick={() => handleAddProduct(flower)} className='order-btn'>
-                            Order now
-                        </button>
-                        <button onClick={() => changeURL(flower.id)} className='order-cart'><i className="fas fa-shopping-cart"></i></button>
-                    </div>
-                    <div className='group-plus'>
-                        <PlusItem text='Bouquest contens' />
-                        <PlusItem text='Details' />
-                        <PlusItem text='Delivery & Pay policy' />
-                    </div>
-
-                    <ToastContainer autoClose={2000} />
-
-                </div>
-
-            ))
-        )
-
-    }
-    return (
-        <div className='infor'>
-            {renderFlower()}
+  const changeURL = (id) => {
+    navigate(`/checkout`);
+  };
+  const renderFlower = () => {
+    return flowers.map((flower) => (
+      <div key={flower.id}>
+        <Heading categoryName={flower.name} text={flower.name} />
+        <Price priceNew={flower.price} />
+        <div className="line"></div>
+        {/* <CountGroup /> */}
+        <div>Color: </div>
+        <ButtonColor />
+        <div className="order">
+          <button
+            onClick={() => handleAddProduct(flower)}
+            className="order-btn"
+          >
+            Order now
+          </button>
+          <button onClick={() => changeURL(flower.id)} className="order-cart">
+            <i className="fas fa-shopping-cart"></i>
+          </button>
         </div>
-    )
-}
+        <div className="group-plus">
+          <PlusItem text="Bouquest contens" />
+          <PlusItem text="Details" />
+          <PlusItem text="Delivery & Pay policy" />
+        </div>
 
+        <ToastContainer autoClose={2000} />
+      </div>
+    ));
+  };
+  return <div className="infor">{renderFlower()}</div>;
+}
 
 export default ProductInfo;
