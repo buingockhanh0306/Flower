01e1d254611fac5565cce92372de1e9768d56eb7
buingockhanh0306 <buikhanhi2k@gmail.com>
2022-07-11 11:27:10 +0700

    change location

--- src/Component/Atoms/Location/index.js
+++ src/Component/Atoms/Location/index.js
@@ -5,7 +5,7 @@ function Location(props) {
     return (
         <div className='location'>
             <div className='location-icon'><i className="fas fa-map-marker-alt"></i></div>
-            <div className='location-text'>London</div>
+            <div className='location-text'>Ha Noi</div>
         </div>
     );
 }
